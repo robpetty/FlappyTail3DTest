a1dabcd1d5d9a5639a73a85c63207622ccb0e432
Robert Petty <liquidgod@hotmail.com>
2020-06-20 15:55:31 -0700

    Bulk commit
    Basic objects, controls, etc.

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=FVXGgPCxvpY
+// https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+// quad click player to follow, unsure why 4 clicks.
+public class PickUpItem : MonoBehaviour
+{
+    public int score = 0;
+    public GameObject winMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Send a ray cast out from player
+        RaycastHit hit;
+        Coin go;
+
+        // "out hit" tells hit what we hit
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 4f))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+
+            go = hit.transform.GetComponent<Coin>();
+
+            if (go == null)
+            {
+                Debug.Log("Not a coin");
+            }
+            else
+            {
+
+                if ((go.isCoin == true) &&
+                    (Input.GetKey("e")))
+                {
+                    Debug.Log("Found coin");
+
+                    score++;
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.white);
+            Debug.Log("Did not Hit");
+        }
+
+        if (score == 3)
+        {
+            // game over
+            GetComponent<PlayerMove>().enabled = false;
+
+            winMessage.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public float mouseSenseX = 1f;
+    public float mouseSenseY = 1f;
+
+    // limit view angle, no 360 noscope
+    float verticalLookRotation;
+
+    Transform mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Awake()
+    {
+        // prevents mouse pointing from moving out of game window
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        mainCam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSenseX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSenseY;
+
+        // limit view 80 degrees up or down
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -80, 80);
+
+        // make camera look up or down
+        mainCam.localEulerAngles = Vector3.left * verticalLookRotation;
+
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= transform.right * Time.deltaTime * moveSpeed;
+        }
+    }
+}
